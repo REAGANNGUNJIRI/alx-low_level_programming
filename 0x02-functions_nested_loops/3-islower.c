51a16498016eaf88cd00fcbb3eda465818b76027
REAGANNGUNJIRI <billreaganngunjiri@gmail.com>
2023-03-21 15:46:57 -0700

    3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,16 +2,15 @@
 
 
 /**
- * _I SLOWER
- * itchecks for lower case character
- * return 1 for lower case character and 0 for anything else
+ * _islower - checks for lowercase characters
+ * Return: 1 for lowercase character 0r 0 for anything else
+ * @c: The character that should be checked
  */
 
 int _islower(int c)
 {
-	if (c>= 97 && c <=122)
+	if (c >= 97 && c <= 122)
 	{
-	
 	return (1);
 	}
 	return (0);
