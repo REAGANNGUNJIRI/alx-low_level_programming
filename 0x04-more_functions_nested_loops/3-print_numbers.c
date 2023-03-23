a40d5628c519e56ddd2ade70c6bcb9a8fb0aeb61
REAGANNGUNJIRI <billreaganngunjiri@gmail.com>
2023-03-23 04:26:43 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print the no from 0 to 9
+ * return: the no since 0 upto 9
+ */
+
+void print_numbers(void)
+
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+	_putchar(x + '0');
+	}
+	_putchar('\n');
+}
