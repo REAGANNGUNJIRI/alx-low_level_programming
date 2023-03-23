cba41be138dae45a5f82d4596dc030f501a8dc19
REAGANNGUNJIRI <billreaganngunjiri@gmail.com>
2023-03-23 05:34:37 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * more_numbers - print 10 times the numbers since 0 upto 14
- * return: 10 times of the numbers since 0 upto 14
+ * Return: 10 times of the numbers since 0 upto 14
  */
 
 void more_numbers(void)
