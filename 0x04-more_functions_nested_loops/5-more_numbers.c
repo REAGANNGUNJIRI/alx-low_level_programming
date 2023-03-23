bc3cb245c64a2c3cd013158ca27a0eee2edf6fa2
REAGANNGUNJIRI <billreaganngunjiri@gmail.com>
2023-03-23 10:10:40 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * more_numbers - print 10 times the numbers since 0 upto 14
- * Return: 10 times of the numbers since 0 upto 14
+ * more_numbers - Print 10 times the numbers since 0 up to 14
+ * Return: 10 times of the numbers since 0 up to 14
  */
 
 void more_numbers(void)
