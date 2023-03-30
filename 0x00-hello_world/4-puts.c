c3715d9acf2ba8363436ecffcc07e7454516077e
REAGANNGUNJIRI <billreaganngunjiri@gmail.com>
2023-03-30 12:55:50 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - C program that prints exactly followed by a new line.
+ * Return: 0
+ */
+
+int main(void)
+
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
+
